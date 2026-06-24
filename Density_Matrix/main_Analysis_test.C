55134f1f3256aa2ec517dc10ca29f09dadbd78df
Feng Liu <feng.liu.2@stonybrook.edu>
2026-06-24 18:03:46 -0400

    job

--- Density_Matrix/main_Analysis_test.C
+++ Density_Matrix/main_Analysis_test.C
@@ -6,31 +6,65 @@
 #include "include/ntp_Lambda_LambdaSelecter.h"
 #include "include/ntp_Lambda_MixTreeReader.h"
 #include <numeric>
-int main(){
+int main(int argc, char*argv[]){
+
+
+	if(argc!=2 ){
+		std::cout<<"Usage: "<<argv[0]<<"file_index"<<std::endl;
+		return 1;
+	}
+	int file_Index = std::stoi(argv[1]);
+
+
+
+
+	std::vector<std::string> FileList;
+	std::vector<std::string> FileName;
+
+	std::ifstream filelist("/gpfs01/star/pwg/fliu/LL_Spin_Correlation/2012data/MiddleTree/filelist.txt");
+	if (!filelist.is_open()) {
+		std::cerr <<"can't open the file" <<std::endl;
+	}
+	std::string directory="/gpfs01/star/pwg/fliu/LL_Spin_Correlation/2012data/MiddleTree/";
+	std::string line;
+	
+	while (std::getline(filelist,line)){
+		FileList.push_back(directory+line);
+		FileName.push_back(line);
+	}
+
+	
+	InputFile_SameEvent.push_back(FileList[file_index]);
+	std::vector<std::string> s1_tmp; 
+	std::vector<std::string> s2_tmp; 
+	s1_tmp.push_back(FileList[file_Index]);
+
+
+
+	std::string MixTreeDir = "/gpfs01/star/pwg/fliu/LL_Spin_Correlation/2012data/MiddleTree_MixTree/";
+	std::string s1 = FileName[file_Index];
+	std::string s2 = FileName[file_Index];
+
+	std::size_t pos = s1.rfind(".root");
+
+	if (pos != std::string::npos) {
+    	s1.erase(pos);
+	}
+
+	
+	s2_tmp.push_back(OutPutDirectory+s1+"/"+s2);
+
+
 
 	std::vector<std::string> InputFile_SameEvent; 
 	std::vector<std::vector<std::string> > InputFile_MixEvent;
 	std::vector<std::vector<std::string> > InputFile_MixTree;
 
 
-	InputFile_SameEvent.push_back("/gpfs01/star/pwg/fliu/LL_Spin_Correlation/2012data/MiddleTree/OutPutLittleTree_2012_OutPut_1_0.root");
-	//InputFile_SameEvent.push_back("/gpfs01/star/pwg/fliu/LL_Spin_Correlation/2012data/MiddleTree/OutPutLittleTree_2012_OutPut_1_1.root");
-	//InputFile_SameEvent.push_back("/gpfs01/star/pwg/fliu/LL_Spin_Correlation/2012data/MiddleTree/OutPutLittleTree_2012_OutPut_1_2.root");
-
-
-	std::vector<std::string> s1_tmp; 
-	s1_tmp.push_back("/gpfs01/star/pwg/fliu/LL_Spin_Correlation/2012data/MiddleTree/OutPutLittleTree_2012_OutPut_1_0.root");
-	s1_tmp.push_back("/gpfs01/star/pwg/fliu/LL_Spin_Correlation/2012data/MiddleTree/OutPutLittleTree_2012_OutPut_1_1.root");
-	s1_tmp.push_back("/gpfs01/star/pwg/fliu/LL_Spin_Correlation/2012data/MiddleTree/OutPutLittleTree_2012_OutPut_1_2.root");
-	std::vector<std::string> s2_tmp; 
-	s2_tmp.push_back("/gpfs01/star/pwg/fliu/LL_Spin_Correlation/2012data/MiddleTree_MixTree/OutPutLittleTree_2012_OutPut_1_0/OutPutLittleTree_2012_OutPut_1_0.root");
-	s2_tmp.push_back("/gpfs01/star/pwg/fliu/LL_Spin_Correlation/2012data/MiddleTree_MixTree/OutPutLittleTree_2012_OutPut_1_0/OutPutLittleTree_2012_OutPut_1_1.root");
-	s2_tmp.push_back("/gpfs01/star/pwg/fliu/LL_Spin_Correlation/2012data/MiddleTree_MixTree/OutPutLittleTree_2012_OutPut_1_0/OutPutLittleTree_2012_OutPut_1_2.root");
-
 	InputFile_MixEvent.push_back(s1_tmp);
 	InputFile_MixTree.push_back(s2_tmp);
 
-	std::string OutPutFile = "MixEvent.root";
+	std::string OutPutFile = std::string("/gpfs01/star/pwg/fliu/LL_Spin_Correlation/2012Result/MixEvent/") + Form("MixEvent_%d.root",file_Index) ;
 
 
 	ntp_Lambda_Reader *mySameEventReader = new ntp_Lambda_Reader(InputFile_SameEvent);
