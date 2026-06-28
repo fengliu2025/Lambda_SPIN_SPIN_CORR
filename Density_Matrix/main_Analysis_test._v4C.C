17093b079000cb60cae8fe93d84f6d6d05319913
Feng Liu <feng.liu.2@stonybrook.edu>
2026-06-28 16:10:55 -0400

    scan set up

--- Density_Matrix/main_Analysis_test._v4C.C
+++ Density_Matrix/main_Analysis_test._v4C.C
@@ -1,4 +1,4 @@
-#include "include/ntp_Lambda_Analyzer_v3.h"
+#include "include/ntp_Lambda_Analyzer_v4.h"
 #include "include/ntp_Lambda_Calculator.h"
 #include "include/ntp_Lambda_Histogram.h"
 #include "include/ntp_Lambda_Reader.h"
@@ -34,72 +34,23 @@ int main(int argc, char*argv[]){
 		FileName.push_back(line);
 	}
 
-	
-
-	std::vector<std::string> s1_tmp; 
-	std::vector<std::string> s2_tmp; 
-	//s1_tmp.push_back(FileList[file_Index]);
-
-
-
-	std::string MixTreeDir = "/gpfs01/star/pwg/fliu/LL_Spin_Correlation/2012data/MiddleTree_MixTree/";
-	std::string s1 = FileName[file_Index];
-
-	/*for(int i=0;i<FileList.size();i++){
-		s1_tmp.push_back(FileList[i]);
-
-
-		std::string s2 = FileName[i];
-
-		std::size_t pos = s1.rfind(".root");
-
-		if (pos != std::string::npos) {
-    		s1.erase(pos);
-		}
-
-	
-		s2_tmp.push_back(MixTreeDir+s1+"/"+s2);
-
-	}
-	*/
-
-	
-	//s1_tmp.push_back(FileList[file_Index]);
-	//s1_tmp.push_back(FileList[0]);
-	s1_tmp.push_back(FileList[0]);
-	//std::string s2 = FileName[file_Index];
-	std::string s2 = FileName[0];
-	std::size_t pos = s1.rfind(".root");
-
-	if (pos != std::string::npos) {
-    	s1.erase(pos);
-	}
-
-	
-	s2_tmp.push_back(MixTreeDir+s1+"/"+s2);
-	//s2_tmp.push_back(MixTreeDir+s1+"/"+FileName[1]);
-	
-	
-
-
-
 	std::vector<std::string> InputFile_SameEvent; 
-	std::vector<std::vector<std::string> > InputFile_MixEvent;
-	std::vector<std::vector<std::string> > InputFile_MixTree;
-
-	InputFile_SameEvent.push_back(FileList[file_Index]);
+	std::vector<std::string> InputFile_MixEvent;
 	
 
-	//for(int i=0; i <FileList.size();i++){
-	//	InputFile_SameEvent.push_back(FileList[i]);
-	//}
 	
-	InputFile_MixEvent.push_back(s1_tmp);
-	InputFile_MixTree.push_back(s2_tmp);
+	
+
+	for(int i=0; i <FileList.size();i++){
+		InputFile_SameEvent.push_back(FileList[i]);
+		InputFile_MixEvent.push_back(FileList[i]);
+	}
+	
+	std::vector<std::vector<std::string>> InputFile_MixTree;
 
 
-	std::string OutPutFile = std::string("/gpfs01/star/pwg/fliu/LL_Spin_Correlation/2012Result/MixEvent/") + Form("MixEvent_%d.root",file_Index) ;
-	//std::string OutPutFile = "SameEvent.root";
+	//std::string OutPutFile = std::string("/gpfs01/star/pwg/fliu/LL_Spin_Correlation/2012Result/MixEvent/") + Form("MixEvent_%d.root",file_Index) ;
+	std::string OutPutFile = "MixEvent_testv4.root";
 
 	ntp_Lambda_Reader *mySameEventReader = new ntp_Lambda_Reader(InputFile_SameEvent);
 	ntp_Lambda_Reader *myMixEventReader  = new ntp_Lambda_Reader(InputFile_MixEvent);
