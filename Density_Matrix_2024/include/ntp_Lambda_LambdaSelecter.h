b499a8726908d8553b695bed94cbdcd656e0f37c
Feng Liu <fliu5@starsub06.sdcc.bnl.gov>
2026-07-04 17:27:01 -0400

    HM

--- Density_Matrix_2024/include/ntp_Lambda_LambdaSelecter.h
+++ Density_Matrix_2024/include/ntp_Lambda_LambdaSelecter.h
@@ -68,7 +68,7 @@ ntp_Lambda_LambdaSelecter::ntp_Lambda_LambdaSelecter()
 
 	trackPtMin  = 0.15;   //GeV/c
 	trackPtMax  = 10000.; //GeV/c
-	trackEtaMax = 1.0;  //1.5;
+	trackEtaMax = 1.5;  //1.5;
 	LambdaPtMin = 0.5;  //GeV/c
 	LambdaPtMax = 5.0;  //GeV/c
 	LambdaRapidityMax =1; //1.5;
