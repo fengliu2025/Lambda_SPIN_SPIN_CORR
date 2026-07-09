63b1cba221968d59779cbf86165f41ffc6a8b1d3
Feng Liu <feng.liu.2@stonybrook.edu>
2026-07-09 14:22:27 -0400

    2012QA

---
+++ QA_Plot_2012/include/ntp_Lambda_EventSelecter.h
@@ -0,0 +1,69 @@
+#ifndef ntp_Lambda_EventSelecter_h
+#define ntp_Lambda_EventSelecter_h
+
+class ntp_Lambda_EventSelecter{
+public:
+	//member variables 
+	std::vector<int> AllowedTriggerId;
+	Float_t VzMax;
+	//memeber functions
+
+	ntp_Lambda_EventSelecter();
+	virtual ~ntp_Lambda_EventSelecter();
+
+
+	bool IsGoodEvent(std::vector<int> EventTrigId,float Vz );
+	bool IsGoodEvent(float Vz);
+	bool IsGoodEvent();
+
+
+};
+
+
+ntp_Lambda_EventSelecter::ntp_Lambda_EventSelecter()
+	: VzMax(60)
+{
+
+}
+
+ntp_Lambda_EventSelecter::~ntp_Lambda_EventSelecter(){
+
+}
+
+
+ bool ntp_Lambda_EventSelecter::IsGoodEvent(std::vector<int> EventTrigId, float Vz){
+ 	bool goodness = false;
+ 	
+ 	for(int i=0 ; i < EventTrigId.size();i++){
+ 		for(int j=0; j< AllowedTriggerId.size();j++){
+ 			if( EventTrigId[i] == AllowedTriggerId[j] ) goodness = true;
+ 		}
+ 	}
+
+
+ 	if(Vz > VzMax || Vz < (-VzMax) ) goodness = false;
+
+ 	return goodness;
+
+ }
+
+ bool ntp_Lambda_EventSelecter::IsGoodEvent(float Vz){
+ 	if( Vz < VzMax && Vz >(-VzMax) ) return true;
+ 	else return false;
+ }
+
+
+
+
+
+ bool ntp_Lambda_EventSelecter::IsGoodEvent(){
+ 	return true;
+ }
+
+
+
+
+
+
+
+#endif
---
+++ QA_Plot_2012/include/ntp_Lambda_Histogram.h
@@ -0,0 +1,251 @@
+#ifndef ntp_Lambda_Histogram_h
+#define ntp_Lambda_Histogram_h
+
+//include the header file here 
+#include <TH1D.h>
+#include <TH2D.h>
+#include <TFile.h>
+#include <TMath.h>
+#include <TLorentzVector.h>
+
+
+#include <string>
+#include <iostream>
+
+
+#include "ntp_Lambda_Reader.h"
+#include "ntp_Lambda_Calculator.h"
+
+#include "constants.h"
+class ntp_Lambda_Histogram {
+public : 
+	ntp_Lambda_Reader *Reader;
+
+	std::string OutPutFile;
+
+	//Histograms for QA plot
+	//QA plots. Basics Distribution of daughter particles.(After Selection).
+
+	TH1D *h1D_Vz;
+	TH1D *h1D_NLambda; 
+
+
+	TH1D *h1D_pair_Pt;
+	TH1D *h1D_pair_Eta;
+	TH1D *h1D_pair_Phi;
+	TH1D *h1D_pair_Mass;
+	TH1D *h1D_pair_Charge;
+	TH1D *h1D_pair_DauDCA;
+	TH1D *h1D_pair_CosTheta;
+	TH1D *h1D_pair_DecayL;
+	TH1D *h1D_pair_DCA;
+
+	TH1D *h1D_p1_Pt;
+	TH1D *h1D_p1_Eta;
+	TH1D *h1D_p1_Phi;
+	TH1D *h1D_p1_Dca;
+
+	TH1D *h1D_p2_Pt;
+	TH1D *h1D_p2_Eta;
+	TH1D *h1D_p2_Phi;
+	TH1D *h1D_p2_Dca;
+	
+
+
+
+	ntp_Lambda_Histogram();
+	ntp_Lambda_Histogram(ntp_Lambda_Reader *reader,std::string outPutFile);
+	void InitHitogram();
+	void Fill_QAplots();
+
+	void Reset();
+
+	void WriteAll();
+
+};
+
+ntp_Lambda_Histogram::ntp_Lambda_Histogram()
+{}
+
+ntp_Lambda_Histogram::ntp_Lambda_Histogram(ntp_Lambda_Reader *reader, ntp_Lambda_Calculator *calculator,std::string outPutFile)
+:Reader(reader),Calculator(calculator),OutPutFile(outPutFile)
+{
+
+}
+
+void ntp_Lambda_Histogram::InitHitogram(){
+	TH1::SetDefaultSumw2(kTRUE);
+
+	h1D_Vz         = new TH1D("h1D_Vz","h1D_vz",200,-100,100);
+	h1D_NLambda    = new TH1D("h1D_NLambda","h1D_NLambda",10,-1,9);
+
+	h1D_pair_Pt    = new TH1D("h1D_pair_Pt","h1D_pair_Pt",100,0,10);
+	h1D_pair_Eta   = new TH1D("h1D_pair_Eta","h1D_pair_Eta",100,-3,3);
+	h1D_pair_Phi   = new TH1D("h1D_pair_Phi","h1D_pair_Phi",100,-2*TMath::Pi(),2*TMath::Pi());
+	h1D_pair_Mass  = new TH1D("h1D_pair_Mass","h1D_pair_Mass",120,1.07,1.20);
+	h1D_pair_Charge= new TH1D("h1D_pair_Charge","h1D_pair_Charge",4,-2,2);
+	h1D_pair_DauDCA= new TH1D("h1D_pair_DauDCA","h1D_pair_DauDCA",100,-5,5);
+	h1D_pair_CosTheta= new TH1D("h1D_pair_CosTheta","h1D_pair_CosTheta",100,0.5,1.5);
+	h1D_pair_DecayL = new TH1D("h1D_piar_DecayL","h1D_pair_DecayL",100,0,30);
+	h1D_pair_DCA    = new TH1D("h1D_pair_DCA","h1D_pair_DCA",100,-2,2);
+
+
+
+
+	h1D_p1_Pt      = new TH1D("h1D_p1_Pt","h1D_p1_Pt",100,0,10);
+	h1D_p1_Eta     = new TH1D("h1D_p1_Eta","h1D_p1_Eta",100,-3,3); 
+	h1D_p1_Phi     = new TH1D("h1D_p1_Phi","h1D_p1_Phi",100,-2*TMath::Pi(),2*TMath::Pi() ); 
+	h1D_p1_Dca     = new TH1D("h1D_p1_Dca","h1D_p1_Dca",100,-1,1);
+
+
+	h1D_p2_Pt      = new TH1D("h1D_p2_Pt","h1D_p2_Pt",100,0,10);
+	h1D_p2_Eta     = new TH1D("h1D_p2_Eta","h1D_p2_Eta",100,-3,3);
+	h1D_p2_Phi     = new TH1D("h1D_p2_Phi","h1D_p2_Phi",100,-2*TMath::Pi(),2*TMath::Pi() ); 
+	h1D_p2_Dca     = new TH1D("h1D_p2_Dca","h1D_p2_Dca",100,-1,1);
+
+
+
+	
+
+	
+	
+
+
+
+}
+
+
+
+void ntp_Lambda_Histogram::Fill_QAplots(){
+		h1D_Vz     			->Fill(Reader->Vz);
+		h1D_NLambda     	->Fill(Reader->NLambda);
+		for(int i =0 ; i < Reader->NLambda;i++){
+			h1D_pair_Pt     	->Fill(Reader->pair_pt[i]);
+			h1D_pair_Eta    	->Fill(Reader->pair_eta[i]);
+			h1D_pair_Phi  		->Fill(Reader->pair_phi[i]);
+			h1D_pair_Mass   	->Fill(Reader->pair_mass[i]);
+			h1D_pair_Charge 	->Fill(Reader->pair_charge[i]);
+			h1D_pair_DauDCA 	->Fill(Reader->pair_DCAdaughters[i]);
+			h1D_pair_CosTheta	->Fill(TMath::Cos(Reader->pair_theta[i] ) );
+			h1D_pair_DecayL 	->Fill(Reader->pair_decayL[i]);
+			h1D_pair_DCA   		->Fill(Reader->pair_decayL[i] * TMath::Sin(Reader->pair_theta[i]) );
+
+
+
+
+			h1D_p1_Pt   		->Fill(Reader->p1_pt[i]);  
+			h1D_p1_Eta    		->Fill(Reader->p1_eta[i]);
+			h1D_p1_Phi    		->Fill(Reader->p1_phi[i]);
+			h1D_p1_Dca    		->Fill(Reader->p1_dca[i]);
+
+
+			h1D_p2_Pt       	->Fill(Reader->p2_pt[i]);
+			h1D_p2_Eta   		->Fill(Reader->p2_eta[i]);
+			h1D_p2_Phi     		->Fill(Reader->p2_phi[i]);
+			h1D_p2_Dca    		->Fill(Reader->p2_dca[i]);
+		}
+
+	
+}
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+void ntp_Lambda_Histogram::Reset(){
+	
+
+
+	h1D_Vz         		->Reset("ICES");
+	h1D_NLambda    		->Reset("ICES");
+
+	h1D_pair_Pt    		->Reset("ICES");
+	h1D_pair_Eta    	->Reset("ICES");
+	h1D_pair_Phi    	->Reset("ICES");
+	h1D_pair_Mass   	->Reset("ICES");
+	h1D_pair_Charge 	->Reset("ICES");
+	h1D_pair_DauDCA 	->Reset("ICES");
+	h1D_pair_CosTheta	->Reset("ICES");
+	h1D_pair_DecayL		->Reset("ICES");
+	h1D_pair_DCA   		->Reset("ICES");
+
+
+
+
+	h1D_p1_Pt     		->Reset("ICES");
+	h1D_p1_Eta    		->Reset("ICES");
+	h1D_p1_Phi    		->Reset("ICES");
+	h1D_p1_Dca    		->Reset("ICES");
+
+
+	h1D_p2_Pt     		->Reset("ICES");
+	h1D_p2_Eta    		->Reset("ICES");
+	h1D_p2_Phi    		->Reset("ICES");
+	h1D_p2_Dca    		->Reset("ICES");
+
+
+
+
+	
+
+	
+}
+
+void ntp_Lambda_Histogram::WriteAll(){
+	TFile *fout= TFile::Open(OutPutFile.c_str(),"RECREATE");
+
+	
+	h1D_Vz         		->Write();
+	h1D_NLambda    		->Write();
+
+	h1D_pair_Pt    		->Write();
+	h1D_pair_Eta    	->Write();
+	h1D_pair_Phi    	->Write();
+	h1D_pair_Mass   	->Write();
+	h1D_pair_Charge 	->Write();
+	h1D_pair_DauDCA 	->Write();
+	h1D_pair_CosTheta	->Write();
+	h1D_pair_DecayL		->Write();
+	h1D_pair_DCA   		->Write();
+
+
+
+
+	h1D_p1_Pt     		->Write();
+	h1D_p1_Eta    		->Write();
+	h1D_p1_Phi    		->Write();
+	h1D_p1_Dca    		->Write();
+
+
+	h1D_p2_Pt     		->Write();
+	h1D_p2_Eta    		->Write();
+	h1D_p2_Phi    		->Write();
+	h1D_p2_Dca    		->Write();
+
+
+	
+
+	fout->Close();
+	delete fout;
+	
+
+	
+	
+
+}
+
+
+
+
+
+
+#endif 
