4f2d581cfe07774d6ebf7c81853aa54ff39f378f
Feng Liu <fliu5@starsub06.sdcc.bnl.gov>
2026-07-17 22:56:02 -0400

    helix

--- QA_Plot_2024/main_Analysis_test.C
+++ QA_Plot_2024/main_Analysis_test.C
@@ -14,11 +14,15 @@ int main(int argc, char*argv[]){
 	std::vector<std::string> FileList;
 	std::vector<std::string> FileName;
 
-	std::ifstream filelist("/gpfs01/star/pwg/fliu/LL_Spin_Correlation/2024data/MiddleTree/filelist.txt");
+	//std::ifstream filelist("/gpfs01/star/pwg/fliu/LL_Spin_Correlation/2024data/MiddleTree/filelist.txt");
+	
+	std::ifstream filelist("/gpfs01/star/pwg/fliu/production/pp2024/PicoDstLambda/filelist.txt");
 	if (!filelist.is_open()) {
 		std::cerr <<"can't open the file" <<std::endl;
 	}
-	std::string directory="/gpfs01/star/pwg/fliu/LL_Spin_Correlation/2024data/MiddleTree/";
+	//std::string directory="/gpfs01/star/pwg/fliu/LL_Spin_Correlation/2024data/MiddleTree/";
+	std::string directory="/gpfs01/star/pwg/fliu/production/pp2024/PicoDstLambda/";
+	
 	std::string line;
 	
 	while (std::getline(filelist,line)){
@@ -46,7 +50,8 @@ int main(int argc, char*argv[]){
 	
 	//std::string OutPutFile = "PreSelectionQA_plot_MB.root"; 
 	//std::string OutPutFile = "AfterSelectionQA_plot_HM_eta1p0.root"; 
-	std::string OutPutFile = "AfterSelectionMass_plot_MB_eta1p0.root"; 
+	//std::string OutPutFile = "AfterSelectionMass_plot_MB_eta1p0.root"; 
+	std::string OutPutFile = "HelixAfterSelectionMass_plot_MB_eta1p0.root"; 
 	ntp_Lambda_Histogram *myHistogram = new ntp_Lambda_Histogram(mySameEventReader,OutPutFile);
 	myHistogram->InitHitogram();
 
