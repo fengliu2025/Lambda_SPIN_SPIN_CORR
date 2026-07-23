50ce9d45552ee19a7dab6cd73dcb1aaffbc6145a
Feng Liu <fliu5@starsub06.sdcc.bnl.gov>
2026-07-23 12:07:45 -0400

    p1p2Pt

--- QA_Plot_2024/main_Analysis_test.C
+++ QA_Plot_2024/main_Analysis_test.C
@@ -51,7 +51,7 @@ int main(int argc, char*argv[]){
 	//std::string OutPutFile = "PreSelectionQA_plot_MB.root"; 
 	//std::string OutPutFile = "AfterSelectionQA_plot_HM_eta1p0.root"; 
 	//std::string OutPutFile = "AfterSelectionMass_plot_MB_eta1p0.root"; 
-	std::string OutPutFile = "P24iyHelixAfterSelectionMass_plot_MB_eta1p0.root"; 
+	std::string OutPutFile = "P24iyHelixAfterSelectionMass_plot_HM_eta1p0.root"; 
 	//std::string OutPutFile = "SLAfterSelectionMass_plot_MB_eta1p0.root"; 
 	ntp_Lambda_Histogram *myHistogram = new ntp_Lambda_Histogram(mySameEventReader,OutPutFile);
 	myHistogram->InitHitogram();
@@ -62,11 +62,11 @@ int main(int argc, char*argv[]){
 
 	ntp_Lambda_EventSelecter *myEventSelecter = new ntp_Lambda_EventSelecter();
 	
-	myEventSelecter->AllowedTriggerId.push_back(910001);
-	myEventSelecter->AllowedTriggerId.push_back(910003);
-	myEventSelecter->AllowedTriggerId.push_back(910013);
-	myEventSelecter->NotAllowedTriggerId.push_back(910802);
-	myEventSelecter->NotAllowedTriggerId.push_back(910804);
+	//myEventSelecter->AllowedTriggerId.push_back(910001);
+	//myEventSelecter->AllowedTriggerId.push_back(910003);
+	//myEventSelecter->AllowedTriggerId.push_back(910013);
+	myEventSelecter->AllowedTriggerId.push_back(910802);
+	myEventSelecter->AllowedTriggerId.push_back(910804);
 	
 	ntp_Lambda_LambdaSelecter *myLambdaSelecter = new ntp_Lambda_LambdaSelecter();
 
