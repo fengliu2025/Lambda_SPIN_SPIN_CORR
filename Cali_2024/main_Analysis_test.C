2f4cac0411785b409057cb75e249ff8c89fadeac
Feng Liu <fliu5@starsub06.sdcc.bnl.gov>
2026-08-01 16:35:20 -0400

    bug

--- Cali_2024/main_Analysis_test.C
+++ Cali_2024/main_Analysis_test.C
@@ -1,6 +1,6 @@
 #include "include/ntp_Lambda_Analyzer_Cali.h"
 #include "include/ntp_Lambda_Histogram.h"
-#include "include/ntp_Lambda_Reader_v2.h"
+#include "include/ntp_Lambda_Reader.h"
 #include "include/ntp_Lambda_EventSelecter.h"
 #include "include/ntp_Lambda_LambdaSelecter.h"
 #include <fstream>
@@ -15,12 +15,12 @@ int main(int argc, char*argv[]){
 	std::vector<std::string> FileName;
 
 	
-	std::ifstream filelist("/gpfs01/star/pwg/fliu/production/pp2024/PicoDstLambda_P24iy_wTrk/FileList.txt");
+	std::ifstream filelist("/gpfs01/star/pwg/fliu/production_test/pp2024_P24iy/PicoDstLambda_P24iy/FileList.txt");
 
 	if (!filelist.is_open()) {
 		std::cerr <<"can't open the file" <<std::endl;
 	}
-	std::string directory="/gpfs01/star/pwg/fliu/production/pp2024/PicoDstLambda_P24iy_wTrk/";
+	std::string directory="/gpfs01/star/pwg/fliu/production_test/pp2024_P24iy/PicoDstLambda_P24iy/";
 	std::string line;
 	
 	while (std::getline(filelist,line)){
@@ -46,7 +46,7 @@ int main(int argc, char*argv[]){
 	ntp_Lambda_Reader *mySameEventReader = new ntp_Lambda_Reader(InputFile_SameEvent);
 	
 	
-	std::string OutPutFile = "Lambda_P24iyHelixScaledMass_plot_eta1p5.root"; 
+	std::string OutPutFile = "Lambda_P24iyScaledMass_plot_eta1p5.root"; 
 	ntp_Lambda_Histogram *myHistogram = new ntp_Lambda_Histogram(mySameEventReader,OutPutFile);
 	myHistogram->Cali_scale_max = 1.000;
 	myHistogram->Cali_scale_min = 0.996;
