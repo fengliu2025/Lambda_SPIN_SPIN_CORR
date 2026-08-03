4d859404db0157387956b47ac8fcb994d3e9bea8
Feng Liu <fliu5@starsub06.sdcc.bnl.gov>
2026-08-03 16:43:31 -0400

    stat

--- EventStat_2024/main_Analysis_test.C
+++ EventStat_2024/main_Analysis_test.C
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <iostream>
 #include <numeric>
 #include <vector>
 #include <string>
@@ -9,23 +10,18 @@
 int main(int argc, char*argv[]){
 
 
-	if(argc!=2 ){
-		std::cout<<"Usage: "<<argv[0]<<"file_index"<<std::endl;
-		return 1;
-	}
-	int file_Index = std::stoi(argv[1]);
-
-
+	
+	
 
 
 	std::vector<std::string> FileList;
 	std::vector<std::string> FileName;
 
-	std::ifstream filelist("/gpfs01/star/pwg/fliu/LL_Spin_Correlation/2024data/MiddleTree/filelist.txt");
+	std::ifstream filelist("/gpfs01/star/pwg/fliu/LL_Spin_Correlation/2024data/FileList.txt");
 	if (!filelist.is_open()) {
 		std::cerr <<"can't open the file" <<std::endl;
 	}
-	std::string directory="/gpfs01/star/pwg/fliu/LL_Spin_Correlation/2024data/MiddleTree/";
+	std::string directory="/star/u/starkong/gpfs01/forFeng/LambdaTreeProduction/pp2024_P24iy/STAR_Production/production/pp2024/PicoDstLambda_P24iy_wTrk/";
 	std::string line;
 	
 	while (std::getline(filelist,line)){
@@ -37,16 +33,26 @@ int main(int argc, char*argv[]){
 	TH1F *hEventStat0 = 0 ;
 	TH1F *hEventStat1 = 0 ;
 
-	for(int i=0 ; i < FileList.size();i++){
-		TFile *fin =TFile::Open();
+	for(int i=0; i < FileList.size();i++){
+		if(i%100==0) std::cout << "i=" << i <<std::endl;
+		TFile *fin =TFile::Open(FileList[i].c_str());
 		TList *list = (TList*)fin->Get("picoLambdaAnaMaker");
 		TH1F*h0 = (TH1F*)list->FindObject("hEventStat0");
 		TH1F*h1 = (TH1F*)list->FindObject("hEventStat1");
-
-
+		h0->GetXaxis()->SetBinLabel(7, "");
+		h1->GetXaxis()->SetBinLabel(7, "");
+		//h0->GetXaxis()->SetNoAlphanumeric();
+		//h1->GetXaxis()->SetNoAlphanumeric();
 		if(i==0){
 			hEventStat0 = (TH1F*)h0->Clone("hEventStat0_sum"); hEventStat0 ->SetDirectory(nullptr);
+		
 			hEventStat1 = (TH1F*)h1->Clone("hEventStat1_sum"); hEventStat1 ->SetDirectory(nullptr);
+		
+			for (int j = 1; j <= h1->GetNbinsX(); ++j) {
+				std::cout << j << " : "
+         		<< h1->GetXaxis()->GetBinLabel(j)
+         		<< std::endl;
+			}
 		}
 
 		else {
@@ -55,12 +61,15 @@ int main(int argc, char*argv[]){
 		}
 
 		fin->Close();
+		delete list;
+		
+		//delete h1;
 		delete fin;
 	}
 
 
 	//-----------
-	std::string OutPutFile = "SameEvent_NEW_ALL.root";   //Same Event
+	std::string OutPutFile = "EventStat.root";   //Same Event
 	//-----------
 	TFile *fout = TFile::Open(OutPutFile.c_str(),"RECREATE");
 
