2dd0cb14895396b342ae63c3e29b17b0384969ef
Feng Liu <feng.liu.2@stonybrook.edu>
2026-08-13 22:39:55 -0400

    test

--- QA_Plot_2024/include/ntp_Lambda_Analyzer_QA.h
+++ QA_Plot_2024/include/ntp_Lambda_Analyzer_QA.h
@@ -282,7 +282,7 @@ void ntp_Lambda_Analyzer::Analysis_QAPlot(){
 					for(int idau1=0;idau1<Dau1Trk.size();idau1++){
 						if( SameEvent_Reader->p1_InEventID[i_lambda] == Dau1Trk[idau1] ) SharedProton ++;
 					}
-					for(int idau2=0;idau2Dau1Trk.size();idau2++){
+					for(int idau2=0;idau2<Dau2Trk.size();idau2++){
 						if( SameEvent_Reader->p2_InEventID[i_lambda] == Dau2Trk[idau2]) SharedPion ++;
 					}
 
