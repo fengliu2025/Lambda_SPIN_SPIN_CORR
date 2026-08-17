dd1118b0ad10382eda38aa6e8a6378d23474b08c
Feng Liu <feng.liu.2@stonybrook.edu>
2026-08-17 16:46:28 -0400

    q

--- Density_Matrix_2024/include/ntp_Lambda_Analyzer_v5.h
+++ Density_Matrix_2024/include/ntp_Lambda_Analyzer_v5.h
@@ -611,7 +611,7 @@ void ntp_Lambda_Analyzer::Analysis_SameEvent(){
 			if( !EventSelecter->IsGoodEvent(TriggerIDList, SameEvent_Reader->Vz) ) continue;
 			int myNLambda =CountMyNLambda();
 			//if(SameEvent_Reader->NLambda !=2  ) continue; // current we only select on multi-Lambdas Events 
-			if(SameEvent_Reader->NLambda <3  ) continue; // current we only select on multi-Lambdas Events 
+			//if(SameEvent_Reader->NLambda <3  ) continue; // current we only select on multi-Lambdas Events 
 			//------------------------Make some selections on the events-----------------------------
 
 			//------------------------Identify Goood Lambda-----------------------------
@@ -875,7 +875,7 @@ void ntp_Lambda_Analyzer::Analysis_MixEvent(){
 			if( !EventSelecter->IsGoodEvent(TriggerIDList,SameEvent_Reader->Vz ) ) continue;
 			int myNLambda =CountMyNLambda();
 			//if(SameEvent_Reader->NLambda != 2) continue;// current we only select on multi-Lambdas Events
-			if(SameEvent_Reader->NLambda <3  ) continue; // current we only select on multi-Lambdas Events 
+			//if(SameEvent_Reader->NLambda <3  ) continue; // current we only select on multi-Lambdas Events 
 			//------------------------Make some selections on the events-----------------------------
 
 
