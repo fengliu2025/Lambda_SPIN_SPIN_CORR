f9f5eaee7a672d07b1812bccc63cbaf9c56f93c4
Feng Liu <feng.liu.2@stonybrook.edu>
2026-09-21 11:39:16 -0400

    q

--- Xi_QA/include/ntp_Lambda_Analyzer_QA.h
+++ Xi_QA/include/ntp_Lambda_Analyzer_QA.h
@@ -292,7 +292,7 @@ void ntp_Lambda_Analyzer::Analysis_QAPlot(){
 				if(SameEvent_Reader->Xi_DCA[i_Xi] > 2 ) continue;
 				if(SameEvent_Reader->DauLambda_mass[i_Xi] < 1.113) continue;
 				if(SameEvent_Reader->DauLambda_mass[i_Xi] > 1.119) continue;
-				if( TMath::Cos( SameEvent_Reader->Xi_theta[i_Xi] ) < 0.9 ) continue;
+				//if( TMath::Cos( SameEvent_Reader->Xi_theta[i_Xi] ) < 0.9 ) continue;
 				if( SameEvent_Reader->pion3_InEventID[i_Xi] == SameEvent_Reader->DauLambda_Dau1_InEventID[i_Xi] ) continue;
 				if( SameEvent_Reader->pion3_InEventID[i_Xi] == SameEvent_Reader->DauLambda_Dau2_InEventID[i_Xi] ) continue;
 				TLorentzVector DauLam; 
